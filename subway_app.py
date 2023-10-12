9ab0113f655cc53880345e8b9a94ed8c2c3bb088
praiz45 <146184799+praiz45@users.noreply.github.com>
2023-10-12 09:14:46 +0000

    application
    app

---
+++ subway_app.py
@@ -0,0 +1,25 @@
+import time
+from subway import player
+from subway import obstacle
+
+class subwayapp:
+    def start():
+        print("starting app....")
+        print("loading")
+        time.sleep(1.0)
+        print("loading")
+        time.sleep(1.0)
+    
+        print("""GHVKJVHEOJHFKETHNGWOKE HOIHETNIOVHJKOVNKJWNVKJ""")
+        time.sleep(3.0)
+        
+subwayapp.start()   
+time.sleep(6.0)
+player_name = input("enter name to start game: ")
+print("starting")
+time.sleep(2.0)
+print(f"starting game with player {player_name}")
+time.sleep(4.0)
+player.run()
+
+
